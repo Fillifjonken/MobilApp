adbe52e173f936312e8834ca98cb559ea7930407
jultomtetris <johan_janssonh@hotmail.com>
2018-05-22 09:55:52 +0200

    Created johans activities (empty)

---
+++ GIFManager/app/src/main/java/example/gifmanager/ScreenFour.java
@@ -0,0 +1,13 @@
+package example.gifmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenFour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_four);
+    }
+}
---
+++ GIFManager/app/src/main/java/example/gifmanager/SignField.java
@@ -0,0 +1,13 @@
+package example.gifmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignField extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_field);
+    }
+}
